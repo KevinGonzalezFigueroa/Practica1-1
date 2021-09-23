14d2ec632237a167f3bd87f76497bd4f371ea8dd
Kevin González <usr2282@salesianos-lacuesta.net>
2021-09-23 14:06:36 +0100

    Feature: Se leen todos los nombres de la lista, metiéndose en un Array, saltando el primer campo, que es la cabecera.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,50 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
+    public final static String separador = ",";
+    public final static String rutaDelArchivo = "C:\\Users\\Kevin\\Desktop\\";
+    public final static String nombreDelArchivo = "Listado de nombres aleatorios.csv";
 
     public static void main(String[] args) {
-	// write your code here
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader((rutaDelArchivo + nombreDelArchivo)));
+
+            String linea = br.readLine();
+
+            String[] campos = linea.split(separador);
+
+            linea = br.readLine();
+
+            while (linea != null) {
+
+                campos = linea.split(separador);
+
+                System.out.println(Arrays.toString(campos));
+
+                linea = br.readLine();
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-}
+    }
+
